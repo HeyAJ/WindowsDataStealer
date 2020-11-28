854b6e09ca7df2be43a67d8564e5af025a491ed4
Aditya Jain <programmer.adityajain999@gmail.com>
2020-11-28 09:11:02 +0530

    Update SystemInfo.c

--- SystemInfo.c
+++ SystemInfo.c
@@ -4,6 +4,6 @@
 int main() 
 { 
 
-    system("C:\\Windows\\System32\\systeminfo.exe >> D:\\main.txt");
+    system("C:\\Windows\\System32\\systeminfo.exe >> Enter Your PATH where you want to save the stealed data for example: D:\\main.txt");
    
 }
